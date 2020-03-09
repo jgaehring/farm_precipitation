5063b05d3bf7348a97372da35ebe6c61ab820a2d
Jamie Gaehring <gaehj457@gmail.com>
2020-03-09 18:16:14 -0400

    Copy weather module from farmOS/farmOS-client.

---
+++ src/FieldModule/Weather/js/components/Widget.vue
@@ -0,0 +1,37 @@
+<template>
+  <widget>
+    <h4 class="card-title">Weather Widget</h4>
+    <h5
+      @click="openWeather"
+      class="clickable">
+      Log Weather
+      <IconRaindrops class="inline-svg"/>
+    </h5>
+  </widget>
+</template>
+
+<script>
+import Widget from '@/components/Widget';
+import IconRaindrops from '@/components/icons/icon-raindrops';
+
+export default {
+  components: {
+    Widget,
+    IconRaindrops,
+  },
+  methods: {
+    openWeather() {
+      this.$router.push({ path: '/weather' });
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+.clickable:hover {
+  background-color: #eee;
+  cursor: pointer;
+}
+
+</style>
