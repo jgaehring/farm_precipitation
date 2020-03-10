e4c2f95cdf5b238f0efabf3ccb3ad245a55406f9
Jamie Gaehring <gaehj457@gmail.com>
2020-03-09 23:41:26 -0400

    Add a basic webpack & eslint config.

---
+++ build/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  entry: './src/FieldModule/Weather/js/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist/FieldModule/Weather'),
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      // Uncomment this to run eslint prior to builds.
+      // {
+      //   enforce: 'pre',
+      //   test: /\.(js|vue)$/,
+      //   loader: 'eslint-loader',
+      //   exclude: /node_modules/
+      // }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
