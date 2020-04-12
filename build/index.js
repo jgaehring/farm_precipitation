4be1bca373b9a6e8903d771fec3bbd3ba51f64c1
Jamie Gaehring <gaehj457@gmail.com>
2020-04-12 15:13:51 -0400

    Load my-logs in dev environment.

--- build/index.js
+++ build/index.js
@@ -1,4 +1,5 @@
 import app from 'farmos-client/src/core/app';
+import logs from 'farmos-client/src/field-modules/my-logs/module.config';
 import precip from '../src/FieldModule/Precipitation/js/module.config';
 
-app('#app', [precip]);
+app('#app', [logs, precip]);
