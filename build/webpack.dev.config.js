394655c9a858952ed9b24e82ea965074e6753b50
Jamie Gaehring <gaehj457@gmail.com>
2020-04-13 22:18:44 -0400

    Add friendly-errors-webpack-plugin to dev config.

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,10 +1,14 @@
 const baseConfig = require('./webpack.config')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
-const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const portfinder = require('portfinder')
 
-module.exports = merge(baseConfig, {
+const HOST = process.env.HOST
+const PORT = process.env.PORT && Number(process.env.PORT)
+
+const devConfig = merge(baseConfig, {
   mode: 'development',
   entry: {
     app: './build/index.js'
@@ -17,6 +21,9 @@ module.exports = merge(baseConfig, {
   devServer: {
     clientLogLevel: 'warning',
     contentBase: false, 
+    host: HOST || 'localhost',
+    hot: true,
+    port: PORT || 8080,
     proxy: [{
       logLevel: 'debug',
       context: [
@@ -26,6 +33,7 @@ module.exports = merge(baseConfig, {
       changeOrigin: true,
       secure: false
     }],
+    quiet: true,
   },
   plugins: [
     new webpack.DefinePlugin({
@@ -43,4 +51,27 @@ module.exports = merge(baseConfig, {
       inject: true
     }),
   ]
+})
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = process.env.PORT || devConfig.devServer.port
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    } else {
+      // publish the new Port, necessary for e2e tests
+      process.env.PORT = port
+      // add port to devServer config
+      devConfig.devServer.port = port
+
+      // Add FriendlyErrorsPlugin
+      devConfig.plugins.push(new FriendlyErrorsPlugin({
+        compilationSuccessInfo: {
+          messages: [`Your application is running here: http://${devConfig.devServer.host}:${port}`],
+        },
+      }))
+
+      resolve(devConfig)
+    }
+  })
 })
