a15ac53d341e6c23f7a6851d76c82ea7fb27278f
Jamie Gaehring <gaehj457@gmail.com>
2020-08-14 08:25:29 -0400

    Remove module.config references.

--- build/index.js
+++ build/index.js
@@ -1,5 +1,5 @@
 import app from 'farmos-client/src/core/app';
-import logs from 'farmos-client/src/field-modules/my-logs/module.config';
-import precip from '../src/FieldModule/Precipitation/js/module.config';
+import logs from 'farmos-client/src/field-modules/my-logs/module';
+import precip from '../src/FieldModule/Precipitation/js/module';
 
 app('#app', [logs, precip]);
